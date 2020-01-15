71fe62185406fccf5ebfb17ce4de6f94570bbbfd
Cherry Wang <cherry@iotechsys.com>
2020-01-15 11:18:57 +0800

    Add timeout and delay when running system management
    Signed-off-by: Cherry Wang <cherry@iotechsys.com>

--- bin/systemManagementTest.sh
+++ bin/systemManagementTest.sh
@@ -9,6 +9,7 @@ echo "Info: Initiating System Management Test."
 echo "[info] ---------- use docker-compose run newman ----------"
 
 docker-compose -f ../docker-compose-test-tools.yml run --rm postman run ${COLLECTION_PATH} \
+    --timeout-script 90000 --timeout-request 90000 --delay-request 4000 \
     --folder="system-management" --environment=${ENV_PATH} --reporters="junit,cli"
 
 echo "Info:System-Management Test Completed."
